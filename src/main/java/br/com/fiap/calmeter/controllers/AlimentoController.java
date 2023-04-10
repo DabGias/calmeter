348d063ec82742b9279cf982296ab4f8c39df8e0
DabGias <gabrielfurlaneti@gmail.com>
2023-04-10 11:41:00 -0300

    adição de pageables

--- src/main/java/br/com/fiap/calmeter/controllers/AlimentoController.java
+++ src/main/java/br/com/fiap/calmeter/controllers/AlimentoController.java
@@ -1,11 +1,12 @@
 package br.com.fiap.calmeter.controllers;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,8 +31,8 @@ public class AlimentoController {
     AlimentoRepository repo;
 
     @GetMapping
-    public List<Alimento> index() {
-        return repo.findAll();
+    public Page<Alimento> index(@PageableDefault(size = 5) Pageable pageable) {
+        return repo.findAll(pageable);
     }
 
     @GetMapping("{id}")
--- src/main/java/br/com/fiap/calmeter/controllers/RefeicaoController.java
+++ src/main/java/br/com/fiap/calmeter/controllers/RefeicaoController.java
@@ -1,11 +1,11 @@
 package br.com.fiap.calmeter.controllers;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,8 +30,8 @@ public class RefeicaoController {
     RefeicaoRepository repo;
 
     @GetMapping
-    public List<Refeicao> index() {
-        return repo.findAll();
+    public Page<Refeicao> index(Pageable pageable) {
+        return repo.findAll(pageable);
     }
 
     @PostMapping
